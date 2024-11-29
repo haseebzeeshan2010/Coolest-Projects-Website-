9452a79e3ee5b40a1b0fbb3d533769e3d808f8e0
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-11-29 17:57:53 +0000

    Add fully responsive navbar and hover effects

--- website/core/views.py
+++ website/core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+# import google.generativeai as genai
+
 
 # Create your views here.
 def index(request):
+    print("eh")
     return render(request, 'core/index.html')
+
+# def ai(request):
+#     genai.configure(api_key="AIzaSyBWtiA3dgEZ7FsWk164cF7SebsdAE9HLzA")
+#     model = genai.GenerativeModel("gemini-1.5-flash")
+#     response = model.generate_content("Give me a healthy omelette recipe").text
+#     print(response)
+#     return render(request, 'core/index.html', response)
