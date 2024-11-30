f6323999975efcd62750a3c17240dfa7c2a2e17c
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-11-30 10:35:32 +0000

    Remove key

--- website/core/views.py
+++ website/core/views.py
@@ -8,7 +8,7 @@ def index(request):
     return render(request, 'core/index.html')
 
 # def ai(request):
-#     genai.configure(api_key="AIzaSyBWtiA3dgEZ7FsWk164cF7SebsdAE9HLzA")
+#     genai.configure(api_key="")
 #     model = genai.GenerativeModel("gemini-1.5-flash")
 #     response = model.generate_content("Give me a healthy omelette recipe").text
 #     print(response)
