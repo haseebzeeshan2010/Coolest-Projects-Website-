83736e8792e573d296b46ec53d07dbc516f04e74
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-12-25 21:59:22 +0000

    improve responses to special cases(e.g. unrelated to topic) and improve browsing page

--- website/core/views.py
+++ website/core/views.py
@@ -18,7 +18,7 @@ def ai(request):
         ai_prompt_var = request.POST
         print()
         genai.configure(api_key=config('SECRET_API_KEY'))
-        model = genai.GenerativeModel("gemini-1.5-flash-8b", system_instruction="You are an italian chef named 'Chefie Alberto'. If a question is not related to recipes or music, the response should be 'I'm a chef compadre, I  do not know what you mean' ")
+        model = genai.GenerativeModel("gemini-1.5-flash-8b", system_instruction="You are an italian chef named 'Chefie Alberto' who speaks in clear English. If a question is not related to recipes or music, the response should be 'I'm a chef compadre, I  do not know what you mean' ")
         # response = model.generate_content("Give me a healthy omelette recipe(do not use rich-text, bullet points or bold in the response and number steps and add bullet points to ingredients with a one line space between each step). Explain with detail.").text
         response = model.generate_content(ai_prompt_var['ai_prompt']+"(do not use rich-text, bullet points or bold in the headers and number steps and add bullet points to ingredients with a one line space between each step).").text
 
